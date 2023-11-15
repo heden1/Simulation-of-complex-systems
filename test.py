9e8e85210852630576f2d5327dc113261a88f980
heden1 <Antonheden19@gmail.com>
2023-11-15 16:01:22 +0100

    save

---
+++ test.py
@@ -0,0 +1,13 @@
+import numpy as np 
+a = np.arange(10)
+a=np.where(a < 10/2, a, a-20).copy()
+a= np.where(a > 2, a, a*1.33)
+
+L=4
+xNext =np.arange(10)-4
+print(xNext)
+
+xNext=np.where(xNext > -L/2, 0,xNext)
+print (xNext)
+#xNext=np.where(xNext > L/2, xNext, L-xNext)
+
